f03ac01f9e2140b04e8cd379618c2a50350e5a30
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-10-20 22:14:25 +0300

    added the C code

---
+++ Smart-National-Park-C-Code/Smart-National-Park-C-Code/main.c
@@ -0,0 +1,18 @@
+/*
+ * Smart-National-Park-C-Code.c
+ *
+ * Created: 20/10/2023 22:13:40
+ * Author : EMMA
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
